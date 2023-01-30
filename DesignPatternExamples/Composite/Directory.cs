a68e8fc970a8948511b9baf5a0bd66a3bbd8908f
f.shahidani <f.shahidani@EP-1230>
2023-01-30 12:14:03 +0330

    composite pattern

---
+++ DesignPatternExamples/Composite/Directory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternExamples.Composite
+{
+    public abstract class FileSystemItem  //component
+    {
+        public abstract long Size();
+    }
+
+    public class File: FileSystemItem  //leaf
+    {
+        private readonly long _size;
+        public override long Size() => _size;
+        public File(long size)
+        {
+            _size = size;
+        }
+    }
+
+    public class Directory: FileSystemItem  //composite
+    {
+        private List<File> Children { get; set; }
+        public Directory(params File[] children)
+        {
+            Children = children.ToList();
+        }
+
+        public override long Size() => Children.Sum(e => e.Size());
+        
+    }
+}
---
+++ DesignPatternsExamples.Tests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatternExamples.Composite;
+using Xunit;
+using Directory = DesignPatternExamples.Composite.Directory;
+using File = DesignPatternExamples.Composite.File;
+
+namespace DesignPatternsExamples.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Get_Size_Of_A_File_System_Item()
+        {
+            var file1 = new File(10);
+            var file2 = new File(20);
+            var file3 = new File(30);
+
+            var directory = new Directory(file1, file2);
+            
+            directory.Size();
+            file1.Size();
+        }
+    }
+}
